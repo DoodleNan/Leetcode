bafeeaebe135dc904a721c0e1216f6b882f348a0
DoodleNan <doodlenan@outlook.com>
2016-09-13 14:21:36 -0400

    Create 326_Power_Of_Three.java

---
+++ 326_Power_Of_Three/326_Power_Of_Three.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n == 0){
+            return false;
+        }
+        while(n%3==0){
+            n = n/3;
+        }
+        
+        if(n == 1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
