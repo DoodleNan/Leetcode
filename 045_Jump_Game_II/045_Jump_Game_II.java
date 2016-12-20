11d29cf8eab9a52a76769135f2bc7a10a07307df
DoodleNan <doodlenan@outlook.com>
2016-12-19 22:56:54 -0500

    Update 045_Jump_Game_II.java

--- 045_Jump_Game_II/045_Jump_Game_II.java
+++ 045_Jump_Game_II/045_Jump_Game_II.java
@@ -1,1 +1,15 @@
-
+public class Solution {
+    public int jump(int[] nums) {
+        int last = 0;
+        int max = nums[0];
+        int step = 0;
+        for(int i = 0; i<nums.length-1; i++){
+            max = Math.max(nums[i]+i, max);
+            if(i==last){
+                step++;
+                last = max;
+            }
+        }
+        return step;
+    }
+}
